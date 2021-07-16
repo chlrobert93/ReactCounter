e5bef2f3617d0bbb52e0610c679bcc3bc57042c1
Eder <chlrobert93@gmail.com>
2021-07-16 12:28:19 -0500

    useState hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import PropTypes from 'prop-types'
 
 
 const CounterApp = ({value}) =>{
 
+    const [nombre, setNombre] = useState('Goku');
+
+    //Cuando usamos el set caounter le decimos a react que el cauter cambio
+    //Por consecuencia hay que dispararla renderización
+    //Solo va rederizar lo que realmente cambia
+
+    const [counter, setCounter] = useState(0); //Retorna un arreglo con dos valores
+
+    console.log(nombre, setNombre); 
+
+
+
     //handleAdd
+    const hanleAdd2 = (e) =>{
+       //  setCounter(counter +1);
+        setCounter((c) => c + 1);
+
+    }
 
     const handleAdd = (e) => {
         console.log(e)
+        
 
         return ()=> console.log('Hola mundo')
     }
@@ -16,8 +34,10 @@ const CounterApp = ({value}) =>{
         <div>
             <h1>Counter</h1>
             <h2>{value}</h2>
+            <h2>{counter}</h2>
             <button onClick={ (e) => {handleAdd(e)}}>+1</button>
             <button onClick={handleAdd()}>+1</button>
+            <button onClick={hanleAdd2}>Botoon2</button>
 
         </div>
     )
