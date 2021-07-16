1938362deb7437e321dbc2443b454c0e15030101
Eder <chlrobert93@gmail.com>
2021-07-16 13:27:46 -0500

    hanleSubtract y hanleReset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import PropTypes from 'prop-types'
 
 
-const CounterApp = ({value}) =>{
+const CounterApp = ({value = 10}) => {
 
     const [nombre, setNombre] = useState('Goku');
 
@@ -10,7 +10,7 @@ const CounterApp = ({value}) =>{
     //Por consecuencia hay que dispararla renderización
     //Solo va rederizar lo que realmente cambia
 
-    const [counter, setCounter] = useState(0); //Retorna un arreglo con dos valores
+    const [counter, setCounter] = useState(value); //Retorna un arreglo con dos valores
 
     console.log(nombre, setNombre); 
 
@@ -18,26 +18,27 @@ const CounterApp = ({value}) =>{
 
     //handleAdd
     const hanleAdd2 = (e) =>{
-       //  setCounter(counter +1);
-        setCounter((c) => c + 1);
+        setCounter(counter +1);
+        //setCounter((c) => c + 1);
 
     }
 
-    const handleAdd = (e) => {
-        console.log(e)
-        
 
-        return ()=> console.log('Hola mundo')
-    }
+    const hadledAdd3 = (e) => setCounter(counter-1);
 
     return(
         <div>
             <h1>Counter</h1>
             <h2>{value}</h2>
             <h2>{counter}</h2>
-            <button onClick={ (e) => {handleAdd(e)}}>+1</button>
-            <button onClick={handleAdd()}>+1</button>
-            <button onClick={hanleAdd2}>Botoon2</button>
+     
+            
+            <button onClick={hanleAdd2}>+1</button>
+             <button onClick={ () => setCounter(value)}>Reset</button>
+             <button onClick={hadledAdd3}>-1</button>
+
+
+
 
         </div>
     )
