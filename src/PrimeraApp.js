6bf96cc3163ca886518ae78d6bcc38a43c0d0e0e
Eder <chlrobert93@gmail.com>
2021-07-15 21:26:18 -0500

    Retornar elementos en el componenente fragment

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,9 +1,17 @@
 import React from 'react';
+//import React, {Fragment} from 'react';
 //FC
 
 const primeraApp = () => {
 
-    return <h1>Hola Mundo</h1>
+    return (
+       
+       <>
+          <h1>Hola Mundo</h1>
+          <p>Mi priemra aplicación</p>
+       </>
+    
+    );
 }
 
 export default primeraApp;
